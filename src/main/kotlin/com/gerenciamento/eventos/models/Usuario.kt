8e2fe18f00e267d1bc3a51382e4db7b153628c02
Vinicius Monteiro <contato.viniciusdanielmonteiro@gmail.com>
2024-11-22 20:14:22 -0400

    commit inicial

---
+++ src/main/kotlin/com/gerenciamento/eventos/models/Usuario.kt
@@ -0,0 +1,4 @@
+package com.gerenciamento.eventos.models
+
+class Usuario {
+}
---
+++ src/test/kotlin/com/gerenciamento/eventos/EventosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gerenciamento.eventos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
